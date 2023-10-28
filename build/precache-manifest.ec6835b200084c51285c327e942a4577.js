b436b6aae42e0cf82d7df4b5565aaa1e71149fda
Vishnu Jangid <vishnu.j8503@gmail.com>
2023-10-28 23:52:06 +0530

    second commit

---
+++ build/precache-manifest.ec6835b200084c51285c327e942a4577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7cb29f2255b3513cc228623e08d1bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37c05e1762d72e0cad84",
+    "url": "/static/css/main.927d3e7b.chunk.css"
+  },
+  {
+    "revision": "14b3df4d5b74640299ec",
+    "url": "/static/js/2.cc991053.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.cc991053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c05e1762d72e0cad84",
+    "url": "/static/js/main.7cfb819b.chunk.js"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "30601a76b57a6c5eb5719398776f9ef7",
+    "url": "/static/media/memorify.30601a76.png"
+  }
+]);
